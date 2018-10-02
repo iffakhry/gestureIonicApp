4ab6aea210843a752c67ac676b8a906e7b82089b
Fakhry Macbook <fakhry.ikhsan.firdaus@gmail.com>
2018-10-02 14:24:25 +0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  public tap: number = 0;
+  public press: number = 0;
+  public pan: number = 0;
+  public swipe: number = 0;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  tapEvent(e) {
+    this.tap++
+  }
+  pressEvent(e) {
+    this.press++
+  }
+  panEvent(e) {
+    this.pan++
+  }
+  swipeEvent(e) {
+    this.swipe++
   }
 
 }
